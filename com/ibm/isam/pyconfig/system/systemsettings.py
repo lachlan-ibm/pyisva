c10f0f0cf550c95cc2c976843abf7d1708e4bf99
Benjamin Martin <benmarti@au1.ibm.com>
2016-12-03 16:19:46 +1000

    Added Configuration to SystemSettings Added the Configuration class to the inheritance list of SystemSettings.

--- com/ibm/isam/pyconfig/system/systemsettings.py
+++ com/ibm/isam/pyconfig/system/systemsettings.py
@@ -8,13 +8,14 @@ import logging
 
 from com.ibm.isam.util.logger import Logger
 
+from .configuration import Configuration9020
 from .network import NetworkSettings9020
 from .secure import SecureSettings9020
 from .system import SystemSettings9020 as SysSettings9020
 from .updates import UpdatesLicensing9020
 
 
-class SystemSettings9020(NetworkSettings9020, SecureSettings9020, SysSettings9020,
+class SystemSettings9020(Configuration9020, NetworkSettings9020, SecureSettings9020, SysSettings9020,
                          UpdatesLicensing9020):
 
     logger = Logger("SystemSettings9020")
