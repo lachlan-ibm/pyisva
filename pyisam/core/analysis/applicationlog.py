a6749eef0788e71ad1fe76418d89c262dc4f64c3
Jasmine Anne Smith <jasmsmit@au1.ibm.com>
2017-02-02 08:58:13 +1000

    Add obligation handling, application log file fetching (#12)
    * Add obligation handling, application log file fetching
    
    * Remove extra logger.debug

--- pyisam/core/analysis/applicationlog.py
+++ pyisam/core/analysis/applicationlog.py
@@ -7,6 +7,7 @@ import logging
 from pyisam.util.model import DataObject
 from pyisam.util.restclient import RESTClient
 
+APPLICATION_LOGS = "/isam/application_logs"
 
 logger = logging.getLogger(__name__)
 
@@ -16,3 +17,14 @@ class ApplicationLog(object):
     def __init__(self, base_url, username, password):
         super(ApplicationLog, self).__init__()
         self.client = RESTClient(base_url, username, password)
+
+    def get_application_log(self, path):
+        parameters = DataObject()
+        parameters.add_value_string("type", "File")
+
+        endpoint = "%s/%s" % (APPLICATION_LOGS, path)
+
+        response = self.client.get_json(endpoint, parameters.data)
+        response.success = response.status_code == 200
+
+        return response
